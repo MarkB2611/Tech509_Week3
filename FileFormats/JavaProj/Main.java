a069c3c6765457324146c647f5b8b55cac3f6ed7
Mark <markblackmore26@gmail.com>
2025-07-17 11:36:42 +0100

    Added
    Added

---
+++ FileFormats/JavaProj/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! 👋");
+
+        // Call a method to demonstrate functionality
+        int result = addNumbers(5, 7);
+        System.out.println("The sum of 5 and 7 is: " + result);
+    }
+
+    // A simple method to add two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
